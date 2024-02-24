de22c11d4f0140424e02389f422cb6073b91eef1
Bevly 0101 <oliveirademonserect@gmail.com>
2024-02-24 19:27:31 -0300

    fix download error

--- src/controllers/download_music.js
+++ src/controllers/download_music.js
@@ -2,19 +2,48 @@ module.exports= async function Dl_Music(req,res){
     const YoutubeMusicApi = require('youtube-music-api')
     const api = new YoutubeMusicApi()
     const ytdl = require('ytdl-core');
-    api.initalize().then(info => {
+    api.initalize().then(async info => {
         if(req.query.type==='name'){
-            api.search(`${req.query.title} official music & lyric`,'video').then(result => {
+            api.search(`${req.query.title} official music & lyric`,'video').then(async result => {
                 const videoID = (result.content.filter(v=>v.type==='video'))[0].videoId
                 res.attachment(`${req.query.title}.mp3`);
-                ytdl(`http://www.youtube.com/watch?v=${videoID}`,{format:'webm', quality:'highestaudio'}).on('data', data=>{}).pipe(res)
-                res.closed
+                const stream = await ytdl(`http://www.youtube.com/watch?v=${videoID}`,{format:'webm', quality:'highestaudio',filter:'audioonly'})
+                //const funcao = stream.listeners('erro')[2];
+                //stream.removeListener('erro',funcao);
+                stream.on('data',data=>{}).pipe(res)
             })
         }
         if(req.query.type==='id'){
             res.attachment(`${req.query.title}.mp3`);
-            ytdl(`http://www.youtube.com/watch?v=${req.query.id}`,{format:'webm', quality:'highestaudio'}).on('data', data=>{}).pipe(res)
-            res.closed
+            ytdl(`http://www.youtube.com/watch?v=${req.query.id}`,{format:'webm',quality:'highestaudio',filter:'audioonly'})
+            .on('data', data=>{}).pipe(res)
+            
         }
     })
 }
+
+
+/**
+ *  const stream await ytdl(url, {
+        quality: highestaudio",
+        filter: 'audioonly",
+        requestOptions: {
+        headers: (
+        cookie: process.env.YOUTUBE_LOGIN_COOKIE,
+    });
+    const audioResource createAudioResource(stream, (
+        inputType: StreamType.Opus,
+    });
+    this.player.play(audioResource);
+    
+    const funcao stream.listeners('error') [2];
+    stream.removeListener('error', funcao);
+    stream.on('error', (err) => {
+    try {
+    throw new Error();
+    } catch {
+    stream.destroy();
+    console.log(err);
+    }
+    });
+ */
