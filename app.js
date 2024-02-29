2dbf8ec064443969ed45470191525fd8e466415a
richards frança <94503486+bevly0101@users.noreply.github.com>
2024-02-29 16:35:15 -0300

    update name

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const App = express();
+const PORT =  process.env.PORT || 3002
+const Routes = require('./src/routes')
+
+App.use(Routes);
+App.use('mp3', express.static(__dirname + '/mp3'));
+
+App.listen(PORT,()=>{
+    console.log(`server rodando na porta ${PORT}`)
+})
